af05caac88c53fb9caca181087c56d4459d7c9f9
ezequiassam <ezequiassam@gmail.com>
2023-07-15 19:18:24 -0300

    Creating models class

--- api/api/settings.py
+++ api/api/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -46,15 +47,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'api.urls'
 
--- api/sales/models.py
+++ api/sales/models.py
@@ -1,3 +1,55 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Transactions(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        null=False,
+        blank=True)
+
+    type = models.CharField(
+        max_length=1,
+        null=False,
+        blank=False)
+
+    date = models.DateTimeField(
+        null=False,
+        blank=False)
+
+    value = models.DecimalField(
+        null=False,
+        blank=False,
+        max_digits=10,
+        decimal_places=2)
+
+    vendor = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False)
+
+
+class Product(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        null=False,
+        blank=True)
+
+    name = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False)
+
+    total = models.DecimalField(
+        null=False,
+        blank=False,
+        max_digits=10,
+        decimal_places=2)
+
+    transactions = models.ManyToManyField(
+        Transactions,
+        verbose_name='list of transactions'
+    )
