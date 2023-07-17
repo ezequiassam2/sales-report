fe56731a7892f20b2ce40928f3d0156200104f99
ezequiassam <ezequiassam@gmail.com>
2023-07-17 14:22:08 -0300

    Creation of the product listing page

--- web/src/components/Dashboard/Dashboard.js
+++ web/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import CollapsibleTable from "./CollapsibleTable";
 
 export default function Dashboard() {
-    return(
+    function createData(product, total) {
+        return {
+            product,
+            total,
+            transactions: [
+                {
+                    date: '2020-01-05',
+                    type: 'Venda',
+                    vendor: 'Jose',
+                    value: 2.4,
+                },
+                {
+                    date: '2020-01-02',
+                    type: 'Comissão',
+                    vendor: 'Maria',
+                    value: 87.4,
+                },
+            ],
+        };
+    }
+
+
+    const rows = [
+        createData('Frozen yoghurt', 159),
+        createData('Ice cream sandwich', 237),
+        createData('Eclair', 262),
+        createData('Cupcake', 305),
+        createData('Gingerbread', 356),
+    ];
+
+    return <>
         <h2>Dashboard</h2>
-    );
+        <CollapsibleTable data={rows}/>
+    </>
 }
---
+++ web/src/components/Dashboard/RowTable.js
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+export default function RowTable(props) {
+    const {row} = props;
+    const [open, setOpen] = React.useState(false);
+
+    return (
+        <React.Fragment>
+            <TableRow sx={{'& > *': {borderBottom: 'unset'}}}>
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    {row.product}
+                </TableCell>
+                <TableCell align="center">{row.total}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{paddingBottom: 0, paddingTop: 0}} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box sx={{margin: 1}}>
+                            <Typography variant="h6" gutterBottom component="div">
+                                Transactions
+                            </Typography>
+                            <Table size="small" aria-label="purchases">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Date</TableCell>
+                                        <TableCell>Type</TableCell>
+                                        <TableCell align="center">Vendor</TableCell>
+                                        <TableCell align="center">Value</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {row.transactions.map((historyRow) => (
+                                        <TableRow key={historyRow.date}>
+                                            <TableCell component="th" scope="row">
+                                                {historyRow.date}
+                                            </TableCell>
+                                            <TableCell>{historyRow.type}</TableCell>
+                                            <TableCell align="center">{historyRow.vendor}</TableCell>
+                                            <TableCell align="center">{historyRow.value}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
+}
+
+RowTable.propTypes = {
+    row: PropTypes.shape({
+        transactions: PropTypes.arrayOf(
+            PropTypes.shape({
+                value: PropTypes.number.isRequired,
+                vendor: PropTypes.number.isRequired,
+                type: PropTypes.string.isRequired,
+                date: PropTypes.string.isRequired,
+            }),
+        ).isRequired,
+        product: PropTypes.string.isRequired,
+        total: PropTypes.number.isRequired,
+    }).isRequired,
+};
